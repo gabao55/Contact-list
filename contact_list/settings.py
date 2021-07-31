3e81395f85cfa8f7b9c7375fedcb37f9a7f26bc1
Gabriel Rosin <39499969+gabao55@users.noreply.github.com>
2021-07-31 19:54:28 -0300

    Update settings.py

--- contact_list/settings.py
+++ contact_list/settings.py
@@ -18,10 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ez!zf_+i@2@3xd$!88cy$)!4!=yg0=ym#c3mv#&@tpry2#z+vw'
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
