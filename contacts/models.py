f7b1e799318d3ecedbb84ec35e5df58e2b6a57f0
Gabriel Rosin <gabriel.s.rosin@gmail.com>
2021-08-02 22:52:45 -0300

    Creating database and making migrations

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+
+"""
+CONTACTS
+name: STR * (mandatory)
+surname: STR * (mandatory)
+telephone: STR * (mandatory)
+email: STR (optional)
+date: DATETIME (automatic)
+description: text
+category: CATEGORY (other model)
+
+CATEGORY:
+name: STR * (mandatory)
+"""
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255, blank=True)
+    telephone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
