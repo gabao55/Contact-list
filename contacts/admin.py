4fb1096cc9cc6dc7be350acd12d829bb6f50699d
Gabriel Rosin <gabriel.s.rosin@gmail.com>
2021-08-03 20:31:57 -0300

    Creating admin and inserting models to the admin page

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'surname', 'telephone', 'email', 'category')
+    list_display_links = ('id', 'name', 'surname')
+    list_filter = ('name', 'surname')
+    list_per_page = 10
+    search_fields = ('name', 'surname', 'email')
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Contact, ContactAdmin)
--- contacts/models.py
+++ contacts/models.py
@@ -19,6 +19,9 @@ name: STR * (mandatory)
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Contact(models.Model):
     name = models.CharField(max_length=255)
@@ -28,3 +31,6 @@ class Contact(models.Model):
     date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
