be7377a058baf463d0131b911e20df08aae1c39b
Gabriel Rosin <gabriel.s.rosin@gmail.com>
2021-08-07 08:45:55 -0300

    Finishing the project

--- contact_list/settings.py
+++ contact_list/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contacts.apps.ContactsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- contacts/admin.py
+++ contacts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Category, Contact
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'surname', 'telephone', 'email', 'category' ,'show')
+    list_display = ('id', 'name', 'surname', 'telephone', 'email', 'category', 'show')
     list_display_links = ('id', 'name', 'surname')
     # list_filter = ('name', 'surname')
     list_per_page = 10
