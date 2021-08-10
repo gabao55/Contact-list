9c2295cde0533a07dd6290bf4b493fa5c623d6f9
Gabriel Rosin <39499969+gabao55@users.noreply.github.com>
2021-08-10 14:56:48 -0300

    Update settings.py

--- contact_list/settings.py
+++ contact_list/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants
+from SECRET_KEY import key
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ay2l9#7(36#!5xge7x-hwngw-tqj&*uic2lo_=(dexc7rv&9qm'
+# SECURITY WARNING: keep the secret key used in production 
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
